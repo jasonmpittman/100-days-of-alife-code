237fcc14088639703ccb694f20a859ab2c233d55
Jason M. Pittman <jason@jasonmpittman>
2018-04-12 09:02:11 -0700

    added main generator loop and started rule engine processor

--- l-system-text.py
+++ l-system-text.py
@@ -7,11 +7,15 @@
 # Explanation: https://en.wikipedia.org/wiki/L-system
 
 import sys
+import re
 
 rules = []
 antecedents = []
 subsequents = []
 seed = "empty"
+system = "empty"
+tempSystem = []
+repeat = True
 
 def process_rules():
 	antecedents, subsequents = zip(*(r.split(" -> ") for r in rules))
@@ -20,6 +24,11 @@ def print_rules():
 	print(antecedents) 
 	print(subsequents)
 
+#where an entry in the system matches an antecedent, change it to the corresponding subsequent
+def process_system(system):
+	for rule in rules:
+		for element in system:
+			tempSystem.append(re.sub(r'element', 'rule'))
 
 print("Please enter rules in the format of A -> B one at a time. Use Ctrl+Z and Enter to end rule input.")
 while True:
@@ -27,10 +36,22 @@ while True:
 		rules.append(input('> '))
 	except EOFError:
 		break
+process_rules()
 
 print("Please enter the seed for the system.")
 seed = input('> ')
 
-process_rules()
+#need 'main' loop for generator
+while repeat:
+	print("Would you like to generate the next sequence in the system? [Y]es or [N]o: ")
+	choice = input()
+
+	if choice == "Y":
+		#process the system
+	else:
+		repeat = False
+
+
+
 
 
