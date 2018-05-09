8512c3020d3ac401e7bd421d65e022b61651627b
jasonmpittman <jmpittman@captechu.edu>
2018-05-08 21:32:36 -0700

    added animation and bounds detection to flock sprite

--- simple-flock.py
+++ simple-flock.py
@@ -5,25 +5,46 @@
 # @license: MIT-license
 # Purpose: example of two agent flocking behavior
 # Explanation:
-
 import pygame
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((640,800))
+
+window_height = 800
+window_width = 600
+
+screen = pygame.display.set_mode((window_width,window_height))
 clock = pygame.time.Clock()
 
-stopped = False
+def main():
+    block_size = 20
+    block_color = (255,255,255)
+    stopped = False
+    velocity = [1, 1]
+    x = window_width / 2
+    y = window_height / 2
 
-while not stopped:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            stopped = True
-    
-        print(event)
-    
-    pygame.display.update()
-    clock.tick(60)
+    while not stopped:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
 
-pygame.quit()
-quit()
+        x += velocity[0]
+        y += velocity[1]
+
+        if x + block_size > window_width or x < 0:
+            velocity[0] = -velocity[0]
+        
+        if y + block_size > window_height or y < 0:
+            velocity[1] = -velocity[1]
+
+        screen.fill((0,0,0))
+        pygame.draw.rect(screen, block_color, [x, y, block_size, block_size])
+        pygame.display.update()
+        clock.tick(60)
+
+
+
+main()
